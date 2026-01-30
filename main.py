5d4dcaf489ed248cdef1caf8e07098c89a99b716
Elchanan_003 <elchanan003@gmail.com>
2026-01-30 13:51:54 +0200

    Level6
    מיזוג כל הפרוייקט לקובץ ראשי
    יצירת פונקציה שמקבלת מקור יעד ורשימת לווינים, ובונה את הנתיב המהיר ביותר לשליחה

--- main.py
+++ main.py
@@ -1,27 +1,154 @@
-from space_network_lib import SpaceNetwork, Packet
-from satellite import Satellite
-from relay_packet import RelayPacket
-from sending_attempt import attempt_transmission, BrokenConnectionError
-from earth import Earth
+import time
+from space_network_lib import *
 
-network = SpaceNetwork(level=5)
+network = SpaceNetwork(level=6)
+MAX_DIST = 150
 
-earth = Earth("Earth", 0)
+# לווין
+class Satellite(SpaceEntity):
+    def __init__(self, name, distance_from_earth):
+        super().__init__(name, distance_from_earth)
 
-sat1 = Satellite("sat1", 100, network)
-sat2 = Satellite("sat2", 200, network)
-sat3 = Satellite("sat3", 300, network)
-sat4 = Satellite("sat4", 400, network)
+    def receive_signal(self, packet: Packet):
+        print(f"[{self.name}] Received: {packet}")
+
+        if isinstance(packet, RelayPacket):
+            inner_packet = packet.data
+            print(f"Unwrapping and forwarding to {inner_packet.receiver}")
+            attempt_transmission(network, inner_packet)
+        else:
+            print(f"Final destination reached: {packet.data}")
+
+# כדור הארץ
+class Earth(SpaceEntity):
+    def __init__(self, name, distance_from_earth):
+        super().__init__(name, distance_from_earth)
 
 
-p_final = Packet("Hello from Earth!!", sat3, sat4)
+    def receive_signal(self, packet: Packet):
+        pass
 
-p_sat2_to_sat3 = RelayPacket(p_final, sat2, sat3)
-p_sat1_to_sat2 = RelayPacket(p_sat2_to_sat3, sat1, sat2)
-p_earth_to_sat1 = RelayPacket(p_sat1_to_sat2, earth, sat1)
+#שגיאה מותאמת אישית
+class BrokenConnectionError(Exception):
+    pass
 
-try:
-    attempt_transmission(network, p_earth_to_sat1)
-except BrokenConnectionError:
-    print("Transmission failed")
+# חבילות ממסר
+class RelayPacket(Packet):
+    def __init__(self, packet_to_relay, sender, proxy):
+        super().__init__(data = packet_to_relay, sender = sender, receiver = proxy)
+
+    def __repr__(self):
+        return f"RelayPacket (Relaying [{self.data}] to {self.receiver} from {self.sender})"
+
+# שליחה כולל טיפול בשגיאות
+def attempt_transmission(manager: SpaceNetwork, packet):
+    while True:
+        try:
+            manager.send(packet)
+            return
+        except TemporalInterferenceError:
+            print("Interference, waiting...")
+            time.sleep(2)
+            continue
+        except DataCorruptedError:
+            print("Data corrupted, retrying...")
+            continue
+        except LinkTerminatedError:
+            print("Link lost")
+            raise BrokenConnectionError()
+        except OutOfRangeError:
+            print("Target out of range")
+            raise BrokenConnectionError()
+
+# מציאת נתיב קצר לשליחת הפאקטה בין גופים רחוקים
+def smart_send_packet(satellites: list, packet:Packet):
+
+    # חישוב מרחק בין המקור ליעד
+    sender_dist = packet.sender.distance_from_earth
+    receiver_dist = packet.receiver.distance_from_earth
+    distance = abs(sender_dist - receiver_dist)
+
+    #אם הטווח המקורי תקין - שולחים את ההודעה מיד
+    if distance <= MAX_DIST:
+        return attempt_transmission(network, packet)
+
+    else:
+        current_receiver = packet.sender
+        route_satellites = []
+
+        while True:
+            # בכל איטרציה הלווין הקולט הופך למקור החדש
+            current_sender = current_receiver
+            #המרחק בין הלווין הנוכחי ליעד הסופי
+            current_dist_to_final = abs(current_sender.distance_from_earth - packet.receiver.distance_from_earth)
+
+            if current_dist_to_final <= MAX_DIST:
+                break
+
+            distance = 0
+            for sat in satellites:
+
+                # המרחק בין השולח ללווין הנוכחי
+                dist_to_sat = abs(sat.distance_from_earth - current_sender.distance_from_earth)
+                #המרחק בין הלווין הנוכחי החדש ליעד הסופי
+                dist_to_final = abs(sat.distance_from_earth - packet.receiver.distance_from_earth)
+
+                #סינון של פרוקסי רלונטי
+                if dist_to_sat == 0:
+                    continue
+                if dist_to_sat > MAX_DIST:
+                    continue
+                if dist_to_final >= current_dist_to_final:
+                    continue
+
+                # בחירה בפרוקסי הכי רחוק שבטווח
+                if dist_to_sat > distance:
+                    current_receiver = sat
+                    distance = dist_to_sat
+
+            if current_sender == current_receiver:
+                break
+
+            # הוספת הלווינים שנבחרו לרשימה סופית
+            route_satellites.append(current_receiver)
+
+
+        # יצירת הודעת ממסר בין הגופים שברשימה הסופית
+        #הודעה פנימית
+        final_pac = Packet(packet.data, route_satellites.pop(), packet.receiver)
+
+        #טיפול במקרה של פרוקסי בודד
+        if len(route_satellites) == 0:
+            relay_pac = RelayPacket(final_pac, packet.sender, final_pac.sender)
+            return attempt_transmission(network, relay_pac)
+
+        #הודעת ממסר לפרוקסי נוסף
+        relay_pac = RelayPacket(final_pac, route_satellites.pop(), final_pac.sender)
+
+        #הוספת קימפולים לפי מספר הגופים שיש ברשימה
+        while len(route_satellites) > 0:
+            relay_pac = RelayPacket(relay_pac, route_satellites.pop(), relay_pac.sender)
+
+        #הוספת שליחה מהשולח המקורי לפרוקסי הראשון
+        final_packet = RelayPacket(relay_pac, packet.sender, relay_pac.sender)
+        return attempt_transmission(network ,final_packet)
+
+
+
+if __name__ == "__main__":
+
+    earth = Earth("Earth", 0)
+
+    sat1 = Satellite("sat1", 100)
+    sat2 = Satellite("sat2", 200)
+    sat3 = Satellite("sat3", 300)
+    sat4 = Satellite("sat4", 400)
+
+    satellites_ = [sat1, sat2, sat3, sat4]
+    packet_ = Packet("Hello from Earth!!", sat3, earth)
+
+    try:
+        smart_send_packet(satellites_, packet_)
+    except BrokenConnectionError:
+        print("Transmission failed")
 
